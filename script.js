748cecd2da6f0ec9342a226bcdfb1aab4a0b4f71
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 22:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+let ele=document.createElement("h1");
+ele.innerText=`You are using ${navigator.appName} version ${navigator.appVersion}`;
+document.getElementById("browser-info").append(ele);
